4ca13bcf5604ab8cac5c7cf9b36759024a1073ff
Aarush Jain <aarushjain33555@gmail.com>
2025-08-05 22:10:15 -0400

    test

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route, useNavigate } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route, useNavigate } from 'react-router-dom';
 import Feature from './Feature';
 import LandingPage from './LandingPage';
 import FilterPage from './FilterPage';
